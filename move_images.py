dd007ae7f302ad7aef8018ec5a07fdc3b1ee7c10
hhp1806 <61499520+hhp1806@users.noreply.github.com>
2020-04-04 19:08:36 +0700

    Add files via upload

---
+++ move_images.py
@@ -0,0 +1,11 @@
+import os
+
+folders = [r'C:\Users\hhp18\OneDrive\Desktop\Console_Dataset\Console Dataset__xbox\img', r'C:\Users\hhp18\OneDrive\Desktop\Console_Dataset\Console Dataset__ playstation\img']
+path = r'C:\Users\hhp18\OneDrive\Desktop\YOLOv3-Series-master\YOLOv3-Series-master\Labelling\images'
+
+
+n = 0
+for folder in folders:
+    for image in os.scandir(folder):
+        n+=1
+        os.rename(image.path, os.path.join(path, '{:06}.jpg'.format(n)))
